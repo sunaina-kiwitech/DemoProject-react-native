14e5bfd71e9e29b7f303cce5817b4e89afcdbf41
sunaina-kiwitech <sunaina.passi@kiwitech.com>
2020-08-11 23:30:32 +0530

    listofsongs

---
+++ component/DashBoard/DashBoard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text
+} from 'react-native';
+import ListOfSongs from './ListOfSongs';
+
+const DashBoard = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.listofsongs}>
+                <ListOfSongs />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: 400, height: 200,
+    },
+    listofsongs: {
+        justifyContent: 'flex-end'
+    }
+});
+
+export default DashBoard;
